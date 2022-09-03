0d6a535bbda0688fa8438b85887085dc283014c0
hugosenari <hugosenari@gmail.com>
2022-09-03 19:52:56 -0300

    feat(nimpkgs): update lock

--- source/tests/test_helpers.nim
+++
@@ -1,9 +0,0 @@
-# this is some template to help mimicking unittest at compile time
-# perhaps we can implement a real compile time unittest?
-
-template ctCheck*(cond: untyped) =
-  doAssert(cond)
-
-template ctTest*(name: string, body: untyped) =
-  body
-  echo "[OK] compile time ", name
